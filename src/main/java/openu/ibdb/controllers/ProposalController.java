28b713fb1b09b5e54fb987e37a8be9264fa4a032
shhadar5 <hadar.shimoni@gmail.com>
2016-04-12 22:09:59 +0300

    add new controllers
    proposal,review

---
+++ src/main/java/openu/ibdb/controllers/ProposalController.java
@@ -0,0 +1,109 @@
+package openu.ibdb.controllers;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import openu.ibdb.models.Proposal;
+import openu.ibdb.repositories.ProposalRepository;
+
+@RestController
+public class ProposalController {
+
+	@RequestMapping("/proposals")
+	public Iterable<Proposal> proposals() {
+		return this.proposalRepository.findAll();
+	}
+
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/proposal/", method = RequestMethod.POST)
+
+	public ResponseEntity<Void> createProposal(@RequestBody Proposal proposal) {
+		System.out.println("Creating proposal " + proposal.getProposalId());
+
+		if (proposalRepository.findOne(proposal.getProposalId()) != null) {
+			System.out.println("A proposal with ID " + proposal.getProposalId() + " already exist");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+
+		proposalRepository.save(proposal);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	// delete author by id http://127.0.0.1:8080/proposal/1
+	@RequestMapping(value = "/proposal/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Proposal> deleteProposal(@PathVariable("id") int id) {
+		System.out.println("Deleting proposal with id " + id);
+
+		Proposal proposal = proposalRepository.findOne(id);
+		if (proposal == null) {
+			System.out.println("Unable to delete. Proposal with id " + id + " not found");
+			return new ResponseEntity<Proposal>(HttpStatus.NOT_FOUND);
+		}
+
+		proposalRepository.delete(id);
+		return new ResponseEntity<Proposal>(HttpStatus.NO_CONTENT);
+	}
+
+	// ------------------- Delete All proposals
+	// --------------------------------------------------------
+
+	@RequestMapping(value = "/proposals/", method = RequestMethod.DELETE)
+	public ResponseEntity<Proposal> deleteAllProposals() {
+		System.out.println("Deleting All Authors");
+
+		proposalRepository.deleteAll();
+		return new ResponseEntity<Proposal>(HttpStatus.NO_CONTENT);
+	}
+
+	// ------------------- Update a proposal
+	// --------------------------------------------------------
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/proposal/", method = RequestMethod.PUT)
+	public ResponseEntity<Void> updateProposal(@RequestBody Proposal proposal) {
+		System.out.println("Updating Proposal " + proposal.getProposalId());
+
+		Proposal myProposal = proposalRepository.findOne(proposal.getProposalId());
+
+		if (myProposal == null) {
+			System.out.println("proposal with id " + proposal.getProposalId() + " not found");
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+
+		myProposal.setAuthorId(proposal.getAuthorId());
+		myProposal.setBookId(proposal.getBookId());
+		myProposal.setUserId(proposal.getUserId());
+		myProposal.setProposalDate(proposal.getProposalDate());
+		
+		proposalRepository.save(myProposal);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	// -------------------Retrieve Single proposal
+	// http://127.0.0.1:8080/author/1--------------------------------------------------------
+
+	@RequestMapping(value = "/proposal/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Proposal> getProposal(@PathVariable("id") int id) {
+		System.out.println("Fetching Proposal with id " + id);
+		Proposal proposal = proposalRepository.findOne(id);
+		if (proposal == null) {
+			System.out.println("proposal with id " + id + " not found");
+			return new ResponseEntity<Proposal>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Proposal>(proposal, HttpStatus.OK);
+	}
+
+	 @Autowired ProposalRepository proposalRepository;
+}
---
+++ src/main/java/openu/ibdb/controllers/ReviewController.java
@@ -0,0 +1,109 @@
+package openu.ibdb.controllers;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import openu.ibdb.models.Review;
+import openu.ibdb.repositories.ReviewRepository;
+
+@RestController
+public class ReviewController {
+
+	@RequestMapping("/reviews")
+	public Iterable<Review> reviews() {
+		return this.reviewRepository.findAll();
+	}
+
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/review/", method = RequestMethod.POST)
+
+	public ResponseEntity<Void> createReview(@RequestBody Review review) {
+		System.out.println("Creating review " + review.getReviewId());
+
+		if (reviewRepository.findOne(review.getReviewId()) != null) {
+			System.out.println("A review with ID " + review.getReviewId() + " already exist");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+
+		reviewRepository.save(review);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	// delete review by id http://127.0.0.1:8080/review/1
+	@RequestMapping(value = "/review/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Review> deleteReview(@PathVariable("id") int id) {
+		System.out.println("Deleting review with id " + id);
+
+		Review review = reviewRepository.findOne(id);
+		if (review == null) {
+			System.out.println("Unable to delete. Review with id " + id + " not found");
+			return new ResponseEntity<Review>(HttpStatus.NOT_FOUND);
+		}
+
+		reviewRepository.delete(id);
+		return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+	}
+
+	// ------------------- Delete All reviews
+	// --------------------------------------------------------
+
+	@RequestMapping(value = "/reviews/", method = RequestMethod.DELETE)
+	public ResponseEntity<Review> deleteAllReviews() {
+		System.out.println("Deleting All Reviews");
+
+		reviewRepository.deleteAll();
+		return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+	}
+
+	// ------------------- Update a review
+	// --------------------------------------------------------
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/review/", method = RequestMethod.PUT)
+	public ResponseEntity<Void> updateReview(@RequestBody Review review) {
+		System.out.println("Updating Review " + review.getReviewId());
+
+		Review myReview = reviewRepository.findOne(review.getReviewId());
+
+		if (myReview == null) {
+			System.out.println("proposal with id " + review.getReviewId() + " not found");
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+
+		myReview.setBookId(review.getBookId());
+		myReview.setUserId(review.getUserId());
+		myReview.setRating(review.getRating());
+		myReview.setReviewComment(review.getReviewComment());
+		
+		reviewRepository.save(myReview);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	// -------------------Retrieve Single review
+	// http://127.0.0.1:8080/review/1--------------------------------------------------------
+
+	@RequestMapping(value = "/review/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Review> getReview(@PathVariable("id") int id) {
+		System.out.println("Fetching Review with id " + id);
+		Review review = reviewRepository.findOne(id);
+		if (review == null) {
+			System.out.println("review with id " + id + " not found");
+			return new ResponseEntity<Review>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Review>(review, HttpStatus.OK);
+	}
+
+	 @Autowired ReviewRepository reviewRepository;
+}
