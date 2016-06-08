59ded1ba22394244086b455bfc93cfbb3d0f6cd1
gulevy <gulevy@cisco.com>
2016-06-09 00:16:51 +0300

    remove unused import

--- src/main/java/openu/ibdb/controllers/ReviewController.java
+++ src/main/java/openu/ibdb/controllers/ReviewController.java
@@ -1,6 +1,5 @@
 package openu.ibdb.controllers;
 
-import org.apache.hadoop.mapred.gethistory_jsp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
